6614cea7e611e6c02b6d7153e2555e71a9859200
Nini <nini@uwu.neko>
2024-07-22 19:45:43 +0400

    finished bookmarks page its literaly PERFECT and as i wanted it to be. the only thing thats left there is to do something about remove button. also fixed the sidebar. its not ideal but it works fine as long as you dont click on it too fast.

--- scripts/bookmark/bookmark input.js
+++ scripts/bookmark/bookmark input.js
@@ -7,21 +7,17 @@ export function addBookmarkEventListeners() {
     addCity.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
             if (addCity.value !== '') {
-                if (bookmarkList.length < 5) {
-                    bookmarkList.push(addCity.value);
-                    addCity.blur();
-                    document.querySelector('.bookmark-add').innerHTML = `
-                        <img src="images/Plus.png" class="icon-plus" alt="">
-                        <div class="bookmark-add-text not-active">Add bookmark</div>
-                    `;
-                    addCity.value = '';
-                    saveBookmarkList();
-                    generateBookmarkHTML();
+                bookmarkList.push(addCity.value);
+                addCity.blur();
+                document.querySelector('.bookmark-add').innerHTML = `
+                    <img src="images/Plus.png" class="icon-plus" alt="">
+                    <div class="bookmark-add-text not-active">Add bookmark</div>
+                `;
+                addCity.value = '';
+                saveBookmarkList();
+                generateBookmarkHTML();
 
-                    addBookmarkClick();
-                } else {
-                    console.log('bookmark limit reached');
-                }
+                addBookmarkClick();
             } else {
                 addCity.blur();
                 document.querySelector('.bookmark-add').innerHTML = `
@@ -38,21 +34,17 @@ export function addBookmarkEventListeners() {
         const inputElement = document.querySelector('.add-bookmark-input');
         if (inputElement) {
             if (inputElement.value !== '') {
-                if (bookmarkList.length < 5) {
-                    bookmarkList.push(addCity.value);
-                    addCity.blur();
-                    document.querySelector('.bookmark-add').innerHTML = `
-                        <img src="images/Plus.png" class="icon-plus" alt="">
-                        <div class="bookmark-add-text not-active">Add bookmark</div>
-                    `;
-                    inputElement.value = '';
-                    saveBookmarkList();
-                    generateBookmarkHTML();
+                bookmarkList.push(addCity.value);
+                addCity.blur();
+                document.querySelector('.bookmark-add').innerHTML = `
+                    <img src="images/Plus.png" class="icon-plus" alt="">
+                    <div class="bookmark-add-text not-active">Add bookmark</div>
+                `;
+                inputElement.value = '';
+                saveBookmarkList();
+                generateBookmarkHTML();
 
-                    addBookmarkClick();
-                } else {
-                    console.log('bookmark limit reached');
-                }
+                addBookmarkClick();
             } else {
                 addCity.blur();
                 document.querySelector('.bookmark-add').innerHTML = `
--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,35 +1,83 @@
-const menu = document.querySelector('.icon-menu');
-const home = document.querySelector('.icon-home');
-const divide = document.querySelector('.icon-divide');
-const bookmark = document.querySelector('.icon-bookmark');
-const alignCenter = document.querySelector('.icon-align-center');
-const settings = document.querySelector('.icon-settings');
+let sidebarActive = false;
 
-menu.addEventListener('click', () => {
-    home.classList.toggle('active');
-    setTimeout(() => {
-        divide.classList.toggle('active');
-    }, 100);
-    setTimeout(() => {
-        bookmark.classList.toggle('active');
-    }, 200);
-    setTimeout(() => {
-        alignCenter.classList.toggle('active');
-    }, 300);
-    setTimeout(() => {
-        settings.classList.toggle('active');
-    }, 1000);
-});
+function initializeSidebar() {
+    const menu = document.querySelector('.icon-menu');
 
-//redirecting
+    menu.addEventListener('click', () => {
+        sidebarActive = !sidebarActive;
 
-home.addEventListener('click', () => {
-    window.location.href = 'home.html';
-});
+        if (sidebarActive) {
+            document.querySelector('.sidebar').innerHTML = `
+                <div class="sidebar-top-section">
+                <img class="icon-menu" src="images/Menu.png" alt="">
+                <div class="home icon-container">
+                    <img class="icon-home icon " src="images/Home.png" alt="">
+                    <div class="icon-home-text icon-text">Home</div>
+                </div>
+                <div class="compare icon-container">
+                    <img class="icon-divide icon" src="images/Divide.png" alt="">
+                    <div class="icon-divide-text icon-text">Compare</div>
+                </div>
+                <div class="bookmark icon-container">
+                    <img class="icon-bookmark icon" src="images/Bookmark.png" alt="">
+                    <div class="icon-bookmark-text icon-text">Bookmark</div>
+                </div>
+                <div class="align-center icon-container">
+                    <img class="icon-align-center icon" src="images/Align center.png" alt="">
+                    <div class="icon-align-center-text icon-text">History</div>
+                </div>
+                </div>
+                <img class="icon-settings icon" src="images/Settings.png" alt="">
+            `;
+            initializeSidebar();
 
-bookmark.addEventListener('click', () => {
-    window.location.href = 'bookmark.html';
-});
+            const home = document.querySelector('.icon-home');
+            const divide = document.querySelector('.icon-divide');
+            const bookmark = document.querySelector('.icon-bookmark');
+            const alignCenter = document.querySelector('.icon-align-center');
+            const settings = document.querySelector('.icon-settings');
+
+            if (home) home.classList.toggle('active');
+            setTimeout(() => { if (divide) divide.classList.toggle('active'); }, 100);
+            setTimeout(() => { if (bookmark) bookmark.classList.toggle('active'); }, 200);
+            setTimeout(() => { if (alignCenter) alignCenter.classList.toggle('active'); }, 300);
+            setTimeout(() => { if (settings) settings.classList.toggle('active'); }, 1000);
+
+            if (home) home.addEventListener('click', () => {
+                window.location.href = 'home.html';
+            });
+
+            if (bookmark) bookmark.addEventListener('click', () => {
+                window.location.href = 'bookmark.html';
+            });
+
+        } else {
+            const home = document.querySelector('.icon-home');
+            const divide = document.querySelector('.icon-divide');
+            const bookmark = document.querySelector('.icon-bookmark');
+            const alignCenter = document.querySelector('.icon-align-center');
+            const settings = document.querySelector('.icon-settings');
+
+            if (home) home.classList.toggle('active');
+            setTimeout(() => { if (divide) divide.classList.toggle('active'); }, 100);
+            setTimeout(() => { if (bookmark) bookmark.classList.toggle('active'); }, 200);
+            setTimeout(() => { if (alignCenter) alignCenter.classList.toggle('active'); }, 300);
+            setTimeout(() => { if (settings) settings.classList.toggle('active'); }, 1000);
+
+            setTimeout(() => {
+                document.querySelector('.sidebar').innerHTML = `
+                <div class="sidebar-top-section">
+                <img class="icon-menu" src="images/Menu.png" alt="">
+                </div>
+                <img class="icon-settings icon" src="images/Settings.png" alt="">
+            `;
+                initializeSidebar();
+            }, 1100);
+        }
+    });
+}
+
+initializeSidebar();
 
 document.querySelector('.image-cloud').addEventListener('click', () => {
     window.location.href = 'home.html';
